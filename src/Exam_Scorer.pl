66fbc657c2ed69cf886230b30c42ab8fca1dbc0e
damjan <damjan.mlinar@students.fhnw.ch>
2024-09-12 20:32:03 +0200

    all files documented now only write the technical documentation

--- src/Exam_Scorer.pl
+++ src/Exam_Scorer.pl
@@ -9,32 +9,40 @@ use Cheating;
 use Regex ':regex';
 use Statistics ':subs';
 
+# Master multiple choice exam, all students and their performance details.
 my $master = undef;
 my @students = ();
 (my $assessment, my %missing_questions, my %instead_questions, my %missing_answers, my %instead_answers, my %result, my %cheaters);
 
+# Calculates the score for each completed multiple choice exam and prints out information about it.
 sub print_score () {
     if ($master && @students) {
         my $question_num = $master->question_amount();
 
+        # For each student, compare the chosen answer with the master file.
         for my $student (@students) {
             my $student_questions = 0;
             my $student_score = 0;
 
+            # Go through each question and compare it with the master solution.
             for my $num (1 .. $question_num) {
-                # hole frage von master, hole exacte oder ähnliche frage von student 
+                
+                # Retrieve the exact or similar question from the student for each question from the master file. 
                 my $norm_question_master = $master->question_through_num ($num);
                 my $norm_question_student = $student->question_through_toleranz ($norm_question_master);
                 
-                # hole gewählte antwort von master und student
+                # Retrieve the selected answer from both the master file and the student’s responses.
                 my @master_answers_marked = $master->marked_answers_through_norm_question($norm_question_master);
                 my @student_answers_marked = $student->marked_answers_through_norm_question($norm_question_student);
                 
-                # überprüfe ob gemeinsame frage angenommen: 1. gleich:gleich (nichts, akzeptieren), gleich:ähnlich (loggen, akzeptieren), gleich:verschieden (loggen, nicht akzeptieren)
+                # Check whether the student has a question.
                 if (defined $norm_question_student) {
+                    
+                    # Check if the student had the exact same question.
                     if ($norm_question_master eq $norm_question_student) {
                         $student_questions++;
                     }
+                    # The student had a question similar to the original one and logging is required.
                     else {
                         $student_questions++;
                         my $missing_q = $master->pretty_question_through_norm($norm_question_master);
@@ -46,11 +54,14 @@ sub print_score () {
                         $instead_questions{$filename_and_question} = $instead_q;
                     }
                     
-                    # falls akzeptiert: # überprüfe ob gemeinsame antwort angenommen: 1. gleich:gleich (nichts, akzeptieren), gleich:ähnlich (loggen, akzeptieren), gleich:verschieden (loggen, nicht akzeptieren)
+                    # Check if the student has provided exactly one answer. If multiple or no answers were provided, the question is considered incorrectly answered.
                     if (scalar(@student_answers_marked) == 1) {
+                        
+                        # Check if the student had the exact same answer as the master file.
                         if ($master_answers_marked[0] eq $student_answers_marked[0]) {
                             $student_score++;
                         }
+                        # Check if the student had a answer similar to the original one. Logging is required if condition is met.
                         elsif (correct_according_distance ($master_answers_marked[0], $student_answers_marked[0])) {
                             $student_score++;
                             my $missing_a = $master->pretty_answer_through_norm($master_answers_marked[0]);
@@ -61,8 +72,8 @@ sub print_score () {
                             push (@{$missing_answers{$filename}}, $missing_a);
                             $instead_answers{$filename_and_answer} = $instead_a;
                         }
+                        # The answer was incorrect and is logged in the cheater object.
                         else {
-                            # Die eine Antwort war falsch
                             my $filename = $student->get_filename();
                             
                             if (not exists $cheaters{$filename}) {
@@ -72,21 +83,19 @@ sub print_score () {
                             $cheaters{$filename}->add_wrong_answer ($norm_question_master, $student_answers_marked[0]);                            
                         }
                     }
-                    else {
-                        # Entweder keine antwort oder mehrere antworten gewaehlt
-                    }
-
-                    # Überprüfe jetzt noch ob alle antworten beim studenten vorhanden sind
+                    
+                    # Now check if the student has provided all the answers.
                     my @master_answers_norm = $master->all_answers_through_norm_question($norm_question_master);
                     my @student_answers_norm = $student->all_answers_through_norm_question($norm_question_student);
                     
                     for my $norm_answer (@master_answers_norm) {
+                        
+                        # Log the answers that are in the master file but missing from the student's multiple choice exam.
                         unless (grep { $_ eq $norm_answer } @student_answers_norm) {
                             my $filename = $student->get_filename();
                             my $missing_a = $master->pretty_answer_through_norm($norm_answer);
                             my $filename_and_answer = $filename . $missing_a;
 
-                            # Überprüfe, ob der Wert $missing_a noch nicht im Array @{$missing_answers{$filename}} ist
                             if (not exists $instead_answers{$filename_and_answer}) {
                                 push (@{$missing_answers{$filename}}, $missing_a);
                             }
@@ -94,6 +103,7 @@ sub print_score () {
                     }
 
                 }
+                # Log the missing question in the student's multiple choice exam.
                 else {
                     my $filename = $student->get_filename();        
                     my $missing_q = $master->pretty_question_through_norm($norm_question_master);
@@ -101,6 +111,7 @@ sub print_score () {
                 }
             }
 
+            # Prepare the final score to be printed on the console.
             my $filename = $student->get_filename();
             my $final_score = $student_score . "/" . $student_questions;
             my $output = $filename . ('.' x (80-length($filename)-length($final_score))) . $final_score;
@@ -108,6 +119,8 @@ sub print_score () {
             $assessment .= "$output\n";
             $result{$filename} = [$student_score, $student_questions];
         }
+
+        # At the end of the calculation, print all the information to the console.
         print_assessment ();
         print_missing_questions ();
         print_missing_answers ();
@@ -119,17 +132,24 @@ sub print_score () {
     }
 }
 
+# Print the achieved score of each student in the multiple choice exam. 
 sub print_assessment () {
+    print '#' x 80 . "\n";
+    print "\n";
     print $assessment;
     print "\n";
     print '#' x 80 . "\n";
     print "\n";
 }
 
+# Print the missing questions of each student in the multiple choice exam. 
 sub print_missing_questions () {
+    
+    # Go through every student who was logged as having missing questions.
     for my $filename (sort keys %missing_questions) {
         print "$filename:\n";
         
+        # Print out the missing questions and any alternative questions used, if applicable.
         for my $question (@{ $missing_questions{$filename} }) {
             print "     Missing question: $question\n";
             my $filename_and_question = $filename . $question;
@@ -146,10 +166,14 @@ sub print_missing_questions () {
     print "\n";
 }
 
+# Print the missing answers of each student in the multiple choice exam. 
 sub print_missing_answers () {
+    
+    # Go through every student who was logged as having missing answers.
     for my $filename (sort keys %missing_answers) {
         print "$filename:\n";
 
+        # Print out the missing answers and any alternative answers used, if applicable.
         for my $answer (@{ $missing_answers{$filename} }) {
             print "     Missing answer: $answer\n";
             my $filename_and_answer = $filename . $answer;
@@ -166,9 +190,11 @@ sub print_missing_answers () {
     print "\n";
 }
 
+# Print statistical information, including the minimum, average, and maximum overall performance of the students.
 sub print_statistics () {
     Statistics::add_students (%result);
     
+    # Calculate statistical information based on the collected data.
     my $avg_question = Statistics::avg_question();
     my @min_question_stats = Statistics::min_question();
     my @max_question_stats = Statistics::max_question();
@@ -180,6 +206,7 @@ sub print_statistics () {
     my %less_half_total_question = Statistics::less_than_half_the_total_question($master->question_amount());
     my %less_half_answers_correct = Statistics::less_than_half_the_answers_correct();
     
+    # Print the minimum, average, and maximum number of answered questions.
     if (scalar(@min_question_stats) == 2 && scalar(@max_question_stats) == 2) {
         my $min_question = $min_question_stats[0];
         my $min_question_amount = $min_question_stats[1];
@@ -192,6 +219,7 @@ sub print_statistics () {
         print "\n";
     }
     
+    # Print the minimum, average, and maximum number of answered questions correctly.
     if (scalar(@min_answer_stats) == 2 && scalar(@max_answer_stats) == 2) {
         my $min_answer = $min_answer_stats[0];
         my $min_answer_amount = $min_answer_stats[1];
@@ -204,8 +232,10 @@ sub print_statistics () {
         print "\n";
     }
 
+    # Ensure that 'Results below expectation:' is printed only once.
     my $header;
 
+    # Print the students who answered fewer than 50% of their questions.
     if (%less_half_total_question) {
         $header = "Results below expectation:\n";
         print $header;
@@ -218,6 +248,7 @@ sub print_statistics () {
         }
     }
 
+    # Print the students who answered fewer than 50% of the questions correctly.
     if (%less_half_answers_correct) {
         if(not defined($header)) {
             $header = "Results below expectation:\n";
@@ -236,18 +267,21 @@ sub print_statistics () {
     print "\n";
 }
 
+# Print the students who have a probability of cheating greater than 50%.
 sub print_cheating ($question_num) {
     my %log_history;
 
+    # Go through every student who was logged as beeing a possible cheater.
     for my $cheater_file (sort keys %cheaters) {
-        
         my $cheater = $cheaters{$cheater_file};
         $cheater->check_if_I_am_cheater ($question_num, %cheaters);
         
+        # Retrieve the logs about cheating for students flagged as a cheater.
         for my $log_name (keys %{$cheater->{log_cheater}}) {
             my $names = $cheater_file . $log_name;
             my $reverse_names = $log_name . $cheater_file;
             
+            # Print out only the two students who have not been printed out yet together.
             unless (exists $log_history{$reverse_names}) {
                 print $cheater->{log_cheater}{$log_name};
                 print "\n";
@@ -260,39 +294,129 @@ sub print_cheating ($question_num) {
     print "\n";
 }
 
+# Accept or deny answers based on whether they are similar, with similarity determined by an edit distance of 10% or less. 
 sub correct_according_distance ($master_string, $student_string) {
     my $edit_distance = distance($master_string, $student_string);
     my $max_toleranz = 0.10 * length($master_string);
     return $edit_distance <= $max_toleranz;
 }
 
+# Create a test-taker object for each file name and load their answers from the exam.
 sub read_in_exams ($master_file,@students_files) {
     $master = Exam_Reader->new($master_file);
-    
     for my $student_file (@students_files) {
         push (@students,Exam_Reader->new($student_file));
     }
 }
 
+# Create a list containing every filename provided through the console arguments.
 sub read_in_files (@inputs) {
     my @files = ();
-    
     for my $input (@inputs) {
         
+        # If it's a single file, add it to the list.
         if (-f $input) {
             push (@files, $input);
-        } 
+        }
+        # Wildcard pattern matches every file that fits the pattern. 
         else {
             my @wild_files = glob ($input);
             push (@files, @wild_files);
        }
-
     }
-
     return @files;
 }
 
+# Read in arguments from the console and execute the assessment.
 my $master_file = $ARGV[0];
 my @students_files = read_in_files(@ARGV[1..scalar(@ARGV)-1]);
 read_in_exams ($master_file,@students_files);
 print_score ();
+
+=head1 NAME 
+
+Exam_Scorer - Reads the master multiple choice exam and a list of completed multiple choice exams, then prints out the assessment.
+
+=head1 USAGE 
+    
+    # To make an assessment:
+    perl src/Exam_Scorer.pl resource/normal-exam/IntroPerlEntryExam.txt resource/normal-exam/*
+    perl src/Exam_Scorer.pl resource/short-exam/IntroPerlEntryExamShort.txt resource/short-exam/*
+    
+=head1 REQUIRED ARGUMENTS 
+
+The first argument should specify the path to the master multiple choice exam file, 
+and the second argument should be a list of completed multiple choice exams.
+
+=head1 DESCRIPTION
+
+This software evaluates multiple choice exams by comparing student responses 
+against a master exam file. It calculates and prints each student's score, 
+identifies missing or incorrect answers, and logs potential cheating instances. 
+Additionally, it provides detailed statistics on student performance.
+
+The multiple-choice exam should look like: 
+
+INSTRUCTIONS:
+...
+
+__________________________________________________
+
+1. First question:
+
+    [X] First answer
+    [ ] Second answer
+    [ ] Third answer
+    ...
+
+...
+
+__________________________________________________
+
+N. Last question:
+
+    [X] Last first answer
+    [ ] Last second answer
+    [ ] Last third answer
+    ...
+
+==================================================
+
+                    END OF EXAM
+
+==================================================
+
+
+=head1 DIAGNOSTICS 
+
+Exams are not loaded: This error occurs if the master exam file 
+or student exam files are not successfully loaded before attempting 
+to calculate scores. Ensure that the file paths provided are correct and that the files are accessible. 
+
+=head1 CONFIGURATION AND ENVIRONMENT 
+
+Exam_Scorer requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES 
+
+Only works under Perl 5.36 and later.
+It requires the following module:
+    
+    Text::Levenshtein
+    Exam_Reader
+    Cheating
+    Regex
+    Statistics
+
+=head1 AUTHOR 
+
+Damjan Mlinar "mlidam@hotmail.com" 
+
+=head1 LICENCE AND COPYRIGHT 
+
+Copyright (c) 2024, Damjan Mlinar "mlidam@hotmail.com". All rights reserved. 
+ 
+This application is free software; you can redistribute it and/or modify it under the same terms as Perl itself. See L<perlartistic>. 
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
